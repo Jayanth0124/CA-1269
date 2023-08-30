7ece6acc22a9e9c2ce5c4222bc8ea146fc95a8bd
Mr.Jayanth <112700471+Jayanth0124@users.noreply.github.com>
2023-08-30 14:03:27 +0530

    Add files via upload

---
+++ decimal to hexadecimal.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int decimalNum;
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimalNum);
+    printf("Hexadecimal representation: %X\n", decimalNum);
+    return 0;
+}
